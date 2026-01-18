f91fdf5a1ec62b614439dfc2954729f517f5b67e
tg1523342 <181992731+hydrazine152@users.noreply.github.com>
2026-01-18 14:11:53 -0500

    Fixed formatting in Configs

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -18,30 +18,34 @@ public final class Configs {
       double nominalVoltage = 12.0;
       double drivingVelocityFeedForward = nominalVoltage / ModuleConstants.kDriveWheelFreeSpeedRps;
 
-      drivingConfig.inverted(ModuleConstants.kDrivingMotorsInverted)
+      drivingConfig
+          .inverted(ModuleConstants.kDrivingMotorsInverted)
           .idleMode(ModuleConstants.kDrivingMotorIdleMode)
           .smartCurrentLimit(ModuleConstants.kDrivingMotorCurrentLimit);
-      drivingConfig.encoder.positionConversionFactor(drivingFactor) // meters
+      drivingConfig.encoder
+          .positionConversionFactor(drivingFactor) // meters
           .velocityConversionFactor(drivingFactor / 60.0); // meters per second
-      drivingConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-          // These are example gains you may need to them for your own robot!
-          .pid(0.04, 0, 0).outputRange(-1, 1).feedForward.kV(drivingVelocityFeedForward);
+      drivingConfig.closedLoop
+          .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+          .pid(0.04, 0, 0).outputRange(-1, 1)
+          .feedForward
+              .kV(drivingVelocityFeedForward);
 
-      turningConfig.inverted(ModuleConstants.kTurningMotorsInverted)
+      turningConfig
+          .inverted(ModuleConstants.kTurningMotorsInverted)
           .idleMode(ModuleConstants.kTurningMotorIdleMode)
           .smartCurrentLimit(ModuleConstants.kTurningMotorCurrentLimit);
 
-      turningConfig.encoder.positionConversionFactor(turningFactor) // rotations
+      turningConfig.encoder
+          .positionConversionFactor(turningFactor) // rotations
           .velocityConversionFactor(turningFactor / 60.0); // rotations per second
 
-      turningConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-          // These are example gains you may need to them for your own robot!
-          .pid(3.5, 0, 0.5).outputRange(-1, 1)
-          // Enable PID wrap around for the turning motor. This will allow the PID
-          // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
-          // to 10 degrees will go through 0 rather than the other direction which is a
-          // longer route.
-          .positionWrappingEnabled(true).positionWrappingInputRange(-0.5, 0.5);
+      turningConfig.closedLoop
+          .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+          .pid(3.5, 0, 0.5)
+          .outputRange(-1, 1)
+          .positionWrappingEnabled(true)
+          .positionWrappingInputRange(-0.5, 0.5);
     }
   }
 }
